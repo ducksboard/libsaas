38255adca3b110d2ff64662a9e7f68e143d03139
Jan Urbański <jan@ducksboard.com>
2012-05-22 17:30:55 +0200

    services: add Zendesk

---
+++ libsaas/services/zendesk/resources.py
@@ -0,0 +1,161 @@
+from libsaas import http, parsers
+from libsaas.services import base
+
+
+def get_params(param_names, param_store):
+    return {name: param_store[name] for name in param_names
+            if param_store.get(name) is not None}
+
+
+class ZendeskResource(base.RESTResource):
+
+    path = None
+
+    @base.apimethod
+    def get(self, page=None, per_page=None):
+        """
+        For single-object resources, fetch the object's data. For collections,
+        fetch all of the objects.
+
+        :var page: For collections, where should paging start. If left as
+            `None`, the first page is returned.
+        :vartype page: int
+
+        :var per_page: For collections, how many objects sould be returned. The
+            maximum is 100. If left as `None`, 100 objects are returned.
+        :vartype per_page: int
+        """
+        params = get_params(('page', 'per_page'), locals())
+        request = http.Request('GET', self.get_url(), params)
+
+        return request, parsers.parse_json
+
+
+class TicketsBase(ZendeskResource):
+
+    path = 'tickets'
+
+
+class Tickets(TicketsBase):
+
+    @base.apimethod
+    def recent(self, page=None, per_page=None):
+        """
+        Fetch all recent tickets. The parameters are the same as for the `get`
+        method.
+        """
+        url = '{0}/{1}'.format(self.get_url(), 'recent')
+        params = get_params(('page', 'per_page'), locals())
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+
+class Ticket(TicketsBase):
+
+    # XXX gives 404 in Zendesk
+    @base.apimethod
+    def collaborators(self, page=None, per_page=None):
+        """
+        Fetch the collaborators on a ticket.
+        """
+        url = '{0}/{1}'.format(self.get_url(), 'collaborators')
+        params = get_params(('page', 'per_page'), locals())
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+
+class UsersBase(ZendeskResource):
+
+    path = 'users'
+
+
+class Users(UsersBase):
+
+    def update(self, *args, **kwargs):
+        raise base.MethodNotSupported()
+
+    def delete(self, *args, **kwargs):
+        raise base.MethodNotSupported()
+
+    @base.apimethod
+    def search(self, query, page=None, per_page=None):
+        """
+        Fetch users based on their usernames or email addresses.
+
+        :var query: A username or an email address.
+        :vartype query: str
+        """
+        url = '{0}/{1}'.format(self.get_url(), 'search')
+        params = get_params(('query', 'page', 'per_page'), locals())
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+
+class CurrentUser(base.HierarchicalResource):
+
+    path = 'users'
+
+    # XXX does not work
+    @base.apimethod
+    def get(self):
+        url = '{0}/{1}'.format(self.get_url(), 'me')
+
+        return http.Request('GET', url), parsers.parse_json
+
+
+class User(UsersBase):
+
+    def create(self, *args, **kwargs):
+        raise base.MethodNotSupported()
+
+    @base.apimethod
+    def tickets_requested(self, page=None, per_page=None):
+        """
+        Fetch tickets requested by this user.
+        """
+        url = '{0}/{1}/tickets/requested.json'.format(self.path,
+                                                      self.object_id)
+        params = self.collection_params(page, per_page)
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+    @base.apimethod
+    def tickets_ccd(self, page=None, per_page=None):
+        """
+        Fetch tickets where this user is CC'd.
+        """
+        url = '{0}/{1}/tickets/ccd.json'.format(self.path, self.object_id)
+        params = self.collection_params(page, per_page)
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+
+class SatisfactionRatingsBase(ZendeskResource):
+
+    path = 'satisfaction_ratings'
+
+    def create(self, *args, **kwargs):
+        raise base.MethodNotSupported()
+
+    def update(self, *args, **kwargs):
+        raise base.MethodNotSupported()
+
+    def delete(self, *args, **kwargs):
+        raise base.MethodNotSupported()
+
+
+class SatisfactionRatings(SatisfactionRatingsBase):
+
+    @base.apimethod
+    def received(self, page=None, per_page=None):
+        """
+        Fetch ratings provided by customers.
+        """
+        url = '{0}/{1}'.format(self.get_url(), 'received')
+        params = get_params(('page', 'per_page'), locals())
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+
+class SatisfactionRating(SatisfactionRatingsBase):
+    pass
---
+++ libsaas/services/zendesk/service.py
@@ -0,0 +1,114 @@
+import json
+
+from libsaas import http, parsers
+
+from libsaas.filters import auth
+from libsaas.services import base
+
+from . import resources
+
+
+class Zendesk(base.Resource):
+    """
+    """
+    def __init__(self, subdomain, username, password):
+        """
+        Create a Zendesk service.
+
+        :var subdomain: The account-specific part of the Zendesk domain, for
+            instance use `mycompany` if your Zendesk domain is
+            `mycompany.zendesk.com`.
+        :vartype subdomain: str
+
+        :var username: The email of the authenticated agent.
+        :vartype username: str
+
+        :var password: The password of the authenticated agent.
+        :vartype password: str
+        """
+        tmpl = 'https://{0}.zendesk.com/api/v2'
+        self.apiroot = tmpl.format(subdomain)
+
+        self.add_filter(auth.BasicAuth(username, password))
+        self.add_filter(self.use_json)
+
+    def get_url(self):
+        return self.apiroot
+
+    def use_json(self, request):
+        request.headers['Content-Type'] = 'application/json'
+        request.headers['Accept'] = 'application/json'
+
+        request.uri += '.json'
+
+        if request.method.upper() not in http.URLENCODE_METHODS:
+            request.params = json.dumps(request.params)
+
+    @base.resource(resources.Tickets)
+    def tickets(self):
+        """
+        Return the resource corresponding to all the tickets.
+        """
+        return resources.Tickets(self)
+
+    @base.resource(resources.Ticket)
+    def ticket(self, ticket_id):
+        """
+        Return the resource corresponding to a single ticket.
+        """
+        return resources.Ticket(self, ticket_id)
+
+    @base.resource(resources.Users)
+    def users(self):
+        """
+        Return the resource corresponding to all users.
+        """
+        return resources.Users(self)
+
+    @base.resource(resources.User, resources.CurrentUser)
+    def user(self, user_id=None):
+        """
+        Return the resource corresponding to a single user. If `user_id` is
+        `None` the returned resource is the currently authenticated user,
+        otherwise it is the user with the given ID number.
+        """
+        if user_id is None:
+            return resources.CurrentUser(self)
+        return resources.User(self, user_id)
+
+    @base.resource(resources.SatisfactionRatings)
+    def satisfaction_ratings(self):
+        """
+        Return the resource corresponding to all satisfaction ratings.
+        """
+        return resources.SatisfactionRatings(self)
+
+    @base.resource(resources.SatisfactionRating)
+    def satisfaction_rating(self, rating_id):
+        """
+        Return the resource corresponding to a single satisfaction rating.
+        """
+        return resources.SatisfactionRating(self, rating_id)
+
+    @base.apimethod
+    def search(self, query, sort_order=None,
+               sort_by=None, page=None, per_page=None):
+        """
+        Fetch the results of a search on your Zendesk account. For details on
+        searching, see
+        http://developer.zendesk.com/documentation/rest_api/search.html
+
+        :var query: A free-form search term.
+        :vartype query: str
+
+        :var sort_order: Optional order in which to sort the results.
+        :vartype query: str
+
+        :var sort_by: Optional term by which to sort the results.
+        :vartype sort_by: str
+        """
+        url = '{0}/{1}'.format(self.get_url(), 'search')
+        params = resources.get_params(('query', 'sort_order', 'sort_by',
+                                       'page', 'per_page'), locals())
+
+        return http.Request('GET', url, params), parsers.parse_json
