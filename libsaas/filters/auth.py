1ad688bceee590ff68e3cf9e074aa1d99a71f2e3
Jan Urbański <jan@ducksboard.com>
2012-05-22 19:15:26 +0200

    filters/auth: use the new string formatting method

--- libsaas/filters/auth.py
+++ libsaas/filters/auth.py
@@ -10,6 +10,6 @@ class BasicAuth(object):
         self.password = password
 
     def __call__(self, request):
-        auth = '%s:%s' % (self.username, self.password)
-        header = 'Basic %s' % base64.b64encode(auth)
+        auth = '{0}:{1}'.format(self.username, self.password)
+        header = 'Basic {0}'.format(base64.b64encode(auth))
         request.headers['Authorization'] = header
