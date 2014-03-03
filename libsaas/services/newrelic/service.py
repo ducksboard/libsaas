24a7f39035bc3bbac13008a4d178d09189fbcb46
Pep Ribas <pepribasp@gmail.com>
2014-03-03 11:03:06 +0100

    newrelic: add rubicon API support

---
+++ libsaas/services/newrelic/service.py
@@ -0,0 +1,77 @@
+import json
+
+from libsaas import http, parsers
+from libsaas.services import base
+
+
+class Rubicon(base.Resource):
+    """
+    """
+    version = 'beta_api'
+
+    def __init__(self, account_id, query_key=None, insert_key=None):
+        """
+        Create a Rubicon service.
+
+        :var account_id: The account id
+        :vartype account_id: str
+
+        :var query_key: The query key.
+        :vartype query_key: str
+
+        :var insert_key: The insert key.
+        :vartype insert_key: str
+        """
+        tmpl = 'https://rubicon.newrelic.com/{0}/accounts/{1}'
+        self.apiroot = tmpl.format(self.version, account_id)
+
+        self.query_key = query_key
+        self.insert_key = insert_key
+
+        self.add_filter(self.add_authorization)
+        self.add_filter(self.use_json)
+
+    def use_json(self, request):
+        request.headers['Content-Type'] = 'application/json'
+        request.headers['Accept'] = 'application/json'
+
+        if request.method.upper() not in http.URLENCODE_METHODS:
+            request.params = json.dumps(request.params)
+
+    def add_authorization(self, request):
+        if request.method.upper() == 'POST':
+            request.headers['X-Insert-Key'] = self.insert_key
+        else:
+            request.headers['X-Query-Key'] = self.query_key
+
+    def get_url(self):
+        return self.apiroot
+
+    @base.apimethod
+    def query(self, nrql):
+        """
+        NRQL query
+
+        :var nqrl: The nrql query
+        :vartype nqrl: str
+
+        Upstream documentation: http://docs.newrelic.com/docs/rubicon/using-nrql
+        """
+        params = base.get_params(None, locals())
+        url = '{0}/{1}'.format(self.get_url(), 'query')
+
+        return http.Request('GET', url, params), parsers.parse_json
+
+    @base.apimethod
+    def insert(self, events):
+        """
+        Submit event or events to rubicon
+
+        :var events: Event data
+        :vartype event: dict
+
+        Upstream documentation: http://docs.newrelic.com/docs/rubicon/inserting-events
+        """
+        url = '{0}/{1}'.format(self.get_url(), 'events')
+
+        return http.Request('POST', url, events), parsers.parse_json
---
+++ test/test_newrelic.py
@@ -0,0 +1,48 @@
+import json
+import unittest
+
+from libsaas.executors import test_executor
+from libsaas.services import newrelic
+
+
+class RubiconTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.executor = test_executor.use()
+        self.executor.set_response(b'{}', 200, {})
+
+        self.insert_key = 'insert_key'
+        self.query_key = 'query_key'
+        self.service = newrelic.Rubicon('account_id', self.query_key,
+                                        self.insert_key)
+
+    def serialize(self, data):
+        return json.dumps(data)
+
+    def expect(self, method=None, uri=None, params=None, headers=None):
+        if method:
+            self.assertEqual(method, self.executor.request.method)
+
+        if self.executor.request.method.upper() == 'POST':
+            self.assertEqual(self.executor.request.headers['X-Insert-Key'],
+                             self.insert_key)
+        else:
+            self.assertEqual(self.executor.request.headers['X-Query-Key'],
+                             self.query_key)
+
+        if uri:
+            self.assertEqual(self.executor.request.uri,
+                             'https://rubicon.newrelic.com/beta_api/' +
+                             'accounts/account_id/' + uri)
+        if params:
+            self.assertEqual(self.executor.request.params, params)
+
+    def test_insert(self):
+        events = [{'eventType': 'test', 'amount': 10}]
+        self.service.insert(events)
+        self.expect('POST', 'events', self.serialize(events))
+
+    def test_sql(self):
+        query = 'select * from TestEvent'
+        self.service.query(query)
+        self.expect('GET', 'query', {'nrql': query})
